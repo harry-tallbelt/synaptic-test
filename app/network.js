f7784d03a6e8ee6035bc16827e6c62108206434b
Igor <harry.tallbelt@gmail.com>
2017-01-08 22:37:04 +0200

    changes to network

--- app/network.js
+++ app/network.js
@@ -9,27 +9,33 @@ const mnist = require('mnist');
 
 
 const trainingOptions = {
-    rate: 0.2,
-    iterations: 10000,
-    error: 0.1,
-    // shuffle: true,
-    // cost: Trainer.cost.CROSS_ENTROPY,
-    log: 1  // log error and every 1st iterations
+    rate: 0.05,
+    iterations: 40,
+    error: 1e-5,
+    log: 1
 };
 
-const perceptron = new Architect.Perceptron(24 * 24, 100, 10);
-const trainer = new Trainer(perceptron, trainingOptions);
-const set = mnist.set(1000, 50);
+const perceptron = new Architect.Perceptron(24 * 24, 40, 10);
+const trainer = new Trainer(perceptron);
+const set = mnist.set(500, 20);
 
-const trainingInfo = trainer.train(set.training);
-console.log(`Training information: ${trainingInfo}.\n`);
+const trainingInfo = trainer.train(set.training, trainingOptions);
 
+const iterations = trainingInfo.iterations;
+const error = trainingInfo.error.toFixed(5);
+const time = (trainingInfo.time / 1000).toFixed(2);
+
+console.log(`\nTraining information: iterations = ${iterations}, error = ${error}, time = ${time}s .\n`);
+
+let hits = 0;
 set.test.forEach(function(item) {
+    const expected = item.output.indexOf(1);
     const result = perceptron.activate(item.input)
-        .map(Math.round);
-        
-    const correct = result.indexOf(1) == item.output.indexOf(1);
-    const symbol = correct ? '+' : '-';
-    
-    console.log(`${symbol} Network computed ${result} for ${item.output}.`);
+        .map(Math.round)
+        .indexOf(1);
+
+    const symbol = expected == result ? (++hits, '+') : '-';
+    console.log(`${symbol} Network thinks it's ${result}, when it's actually ${expected}.`);
 });
+
+console.log(`\nSo it's ${hits} out of ${set.test.length}.`);
